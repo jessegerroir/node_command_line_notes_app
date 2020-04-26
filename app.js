3e475d6970b35149592e38489b3d58db17eb21f7
Jesse Gerroir <jessegerroir@gmail.com>
2020-04-26 13:15:52 -0700

    First commit
    Commiting all the project files thus far. Created the project and worked on it a bit before setting up the repository.

---
+++ app.js
@@ -0,0 +1,64 @@
+
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNotes(argv.title, argv.body)
+    }
+})
+
+// Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNotes(argv.title);
+    }
+})
+
+// List out all the notes
+yargs.command({
+    command: 'list',
+    describe: 'List out all the notes',
+    handler: function () {
+        console.log('Listing the note')
+    }
+})
+
+// Read a note
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading the note')
+    }
+})
+
+yargs.parse()
---
+++ notes.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+
+const getNotes = function(){
+    return "This is a note!";
+}
+
+const addNotes = function (title, body){
+    const notes = loadNotes()
+    
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added')
+    } else {
+        console.log('Note title taken')
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+const removeNotes = function (title) {
+    console.log(title);
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNotes
+}
